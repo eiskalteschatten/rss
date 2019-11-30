a36607c8304ee3f83c3e7039d79199209c844dd5
Alex <alex@alexseifert.com>
2019-11-30 23:28:37 +0100

    Fix linting

--- server/src/services/feedsService.ts
+++ server/src/services/feedsService.ts
@@ -8,73 +8,16 @@ import Article from '../models/Article';
 import Feed from '../models/Feed';
 
 export async function refreshAllFeeds(): Promise<ArticleInterface[]> {
-  try {
-    const rssParser = new RssParser();
+  const rssParser = new RssParser();
 
-    const feeds = await Feed.findAll({
-      attributes: ['id', 'feedUrl']
-    });
+  const feeds = await Feed.findAll({
+    attributes: ['id', 'feedUrl']
+  });
 
-    const newArticles = [];
-    const guids = [];
+  const newArticles = [];
+  const guids = [];
 
-    for (const feed of feeds) {
-      const parsedFeed = await rssParser.parseURL(feed.feedUrl);
-
-      for (const article of parsedFeed.items) {
-        const articleExists = await Article.findOne({
-          where: {
-            guid: article.guid
-          }
-        });
-
-        if (!articleExists) {
-          guids.push(article.guid);
-
-          newArticles.push({
-            ...article,
-            read: false,
-            fkFeed: feed.id
-          });
-        }
-      }
-    }
-
-    await Article.bulkCreate(newArticles);
-
-    const articles = await Article.findAll({
-      where: {
-        read: false,
-        guid: {
-          [Op.in]: guids
-        }
-      },
-      order: [
-        ['pubDate', 'DESC']
-      ]
-    });
-
-    return articles;
-  }
-  catch(error) {
-    throw error;
-  }
-}
-
-export async function refreshForSingleFeed(feedId: number): Promise<ArticleInterface[]> {
-  try {
-    const rssParser = new RssParser();
-
-    const feed = await Feed.findByPk(feedId, {
-      attributes: ['id', 'feedUrl']
-    });
-
-    if (!feed) {
-      throw new HttpError('Feed not found!', 404);
-    }
-
-    const newArticles = [];
-    const guids = [];
+  for (const feed of feeds) {
     const parsedFeed = await rssParser.parseURL(feed.feedUrl);
 
     for (const article of parsedFeed.items) {
@@ -94,25 +37,72 @@ export async function refreshForSingleFeed(feedId: number): Promise<ArticleInter
         });
       }
     }
+  }
 
-    await Article.bulkCreate(newArticles);
+  await Article.bulkCreate(newArticles);
 
-    const articles = await Article.findAll({
+  const articles = await Article.findAll({
+    where: {
+      read: false,
+      guid: {
+        [Op.in]: guids
+      }
+    },
+    order: [
+      ['pubDate', 'DESC']
+    ]
+  });
+
+  return articles;
+}
+
+export async function refreshForSingleFeed(feedId: number): Promise<ArticleInterface[]> {
+  const rssParser = new RssParser();
+
+  const feed = await Feed.findByPk(feedId, {
+    attributes: ['id', 'feedUrl']
+  });
+
+  if (!feed) {
+    throw new HttpError('Feed not found!', 404);
+  }
+
+  const newArticles = [];
+  const guids = [];
+  const parsedFeed = await rssParser.parseURL(feed.feedUrl);
+
+  for (const article of parsedFeed.items) {
+    const articleExists = await Article.findOne({
       where: {
-        read: false,
-        fkFeed: feedId,
-        guid: {
-          [Op.in]: guids
-        }
-      },
-      order: [
-        ['pubDate', 'DESC']
-      ]
+        guid: article.guid
+      }
     });
 
-    return articles;
+    if (!articleExists) {
+      guids.push(article.guid);
+
+      newArticles.push({
+        ...article,
+        read: false,
+        fkFeed: feed.id
+      });
+    }
   }
-  catch(error) {
-    throw error;
-  }
+
+  await Article.bulkCreate(newArticles);
+
+  const articles = await Article.findAll({
+    where: {
+      read: false,
+      fkFeed: feedId,
+      guid: {
+        [Op.in]: guids
+      }
+    },
+    order: [
+      ['pubDate', 'DESC']
+    ]
+  });
+
+  return articles;
 }
