2454f5e9675d3d473d30552831888edfc4f9ff1d
Alex <alex@alexseifert.com>
2019-11-30 20:52:42 +0100

    Fix linting

--- server/src/controllers/api/rss.ts
+++ server/src/controllers/api/rss.ts
@@ -7,37 +7,37 @@ import { HttpError } from '../../lib/Error';
 
 
 class RssController implements Controller {
-    public router: Router;
+  public router: Router;
 
-    public constructor(router: Router) {
-        this.router = router;
-        this.initilizeRoutes();
+  public constructor(router: Router) {
+    this.router = router;
+    this.initilizeRoutes();
+  }
+
+  private initilizeRoutes(): void {
+    this.router.get('/', this.getIndex);
+  }
+
+  private getIndex(req: Request, res: Response): void {
+    const random: number = Math.floor(Math.random() * Math.floor(2));
+
+    // If random === 0, throw an error just for demonstration purposes
+    if (random === 0) {
+      const error: HttpError = {
+        name: 'Teapot Problem',
+        status: 418,
+        message: 'I am a teapot'
+      };
+
+      return returnError(error, req, res);
     }
 
-    private initilizeRoutes(): void {
-        this.router.get('/', this.getIndex);
-    }
-
-    private getIndex(req: Request, res: Response): void {
-        const random: number = Math.floor(Math.random() * Math.floor(2));
-
-        // If random === 0, throw an error just for demonstration purposes
-        if (random === 0) {
-            const error: HttpError = {
-                name: 'Teapot Problem',
-                status: 418,
-                message: 'I am a teapot'
-            };
-
-            return returnError(error, req, res);
-        }
-
-        res.json({
-            message: 'success'
-        });
-    }
+    res.json({
+      message: 'success'
+    });
+  }
 }
 
 export default (router: Router): void => {
-    new RssController(router);
+  new RssController(router);
 };
--- server/src/lib/apiErrorHandling.ts
+++ server/src/lib/apiErrorHandling.ts
@@ -3,6 +3,6 @@ import { Request, Response } from 'express';
 import { HttpError } from './Error';
 
 export function returnError(error: HttpError, req: Request, res: Response): void {
-    console.error(error);
-    res.status(error.status || 500).send(error.message || 'An unknown error occurred!');
+  console.error(error);
+  res.status(error.status || 500).send(error.message || 'An unknown error occurred!');
 }
