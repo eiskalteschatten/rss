7163579317ef44d2430a30b00f006efd801db973
Alex <alex@alexseifert.com>
2019-12-01 13:05:24 +0100

    Fix linting

--- server/src/lib/setupPassport.ts
+++ server/src/lib/setupPassport.ts
@@ -3,11 +3,10 @@ import { BasicStrategy } from 'passport-http';
 
 export default function setupPassport(): void {
   passport.use(new BasicStrategy((username, password, done): void => {
-      if (username === process.env.BASIC_AUTH_USERNAME && password === process.env.BASIC_AUTH_PASSWORD) {
-        return done(null, true);
-      }
-
-      return done(null, false);
+    if (username === process.env.BASIC_AUTH_USERNAME && password === process.env.BASIC_AUTH_PASSWORD) {
+      return done(null, true);
     }
-  ));
+
+    return done(null, false);
+  }));
 }
