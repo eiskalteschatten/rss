d2bd23c3b2602cda1e093b03072535d8256deb09
Alex <alex@alexseifert.com>
2019-12-01 14:53:54 +0100

    Add bootstrap script

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 
 import { ThemeProvider } from '@material-ui/styles';
@@ -9,11 +9,15 @@ import GeneralError from './components/GeneralError';
 
 import theme from './theme';
 import store from './store'
-
+import bootstrap from './bootstrap';
 
 import './App.css';
 
 const App: React.FC = () => {
+  useEffect(() => {
+    bootstrap();
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
