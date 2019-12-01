b9a7381a6bc26ad2289f37d40939b44bae9475f3
Alex <alex@alexseifert.com>
2019-12-01 14:34:49 +0100

    Add basic app structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+// import { Provider } from 'react-redux';
+
+import { ThemeProvider } from '@material-ui/styles';
+import { CssBaseline }  from '@material-ui/core';
+
+import GeneralWrapper from './GeneralWrapper';
+import GeneralError from './components/GeneralError';
+import theme from './theme';
+
 import './App.css';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+        {/* <Provider store={store}> */}
+          <GeneralWrapper />
+          <GeneralError />
+        {/* </Provider> */}
+    </ThemeProvider>
   );
 }
 
---
+++ client/src/theme.ts
@@ -0,0 +1,20 @@
+import { blue, red } from '@material-ui/core/colors';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: blue[600]
+    },
+    secondary: {
+      main: '#ffffff'
+    },
+    error: {
+      main: red[900],
+      light: red[600]
+    }
+  }
+});
+
+export default theme;
