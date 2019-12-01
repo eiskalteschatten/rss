ea6cc09d1288386a0be239bfa84fa446661761f6
Alex <alex@alexseifert.com>
2019-12-01 13:10:55 +0100

    Comment out DB migrations until they are needed

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,7 +1,7 @@
 import { Sequelize, Options } from 'sequelize';
 import config from 'config';
-import path from 'path';
-import migrateDb from 'sequelize-migration-wrapper';
+// import path from 'path';
+// import migrateDb from 'sequelize-migration-wrapper';
 
 const dbConfig = config.get<Options>('dbConfig');
 
@@ -11,18 +11,18 @@ const options = Object.assign({}, dbConfig);
 const sequelize = new Sequelize(options);
 export default sequelize;
 
-migrateDb({
-  sequelize,
-  path: path.resolve(__dirname, './migrations'),
-  filePattern: /\.ts|\.js$/
-});
+// migrateDb({
+//   sequelize,
+//   path: path.resolve(__dirname, './migrations'),
+//   filePattern: /\.ts|\.js$/
+// });
 
 export async function setupSequelize(): Promise<Sequelize> {
   try {
     await sequelize.authenticate();
     console.log('Connection to the database has been established successfully.');
 
-    await migrateDb.migrate();
+    // await migrateDb.migrate();
 
     console.log('Database migration scripts successfully executed.');
   }
